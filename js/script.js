79f8a52e151ac75528b8d47beceb13e176026aee
michclleg <mgonzalez18@hawk.iit.edu>
2025-04-17 19:08:59 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 function map() {
-  var mapStart = {
-    center: {lat:41 ,lng:-87},
+  const loc = { lat:41, lng:-87};
+
+  const map = new google.maps.Map(document.getElementById("map"), {
     zoom:5,
-  };
-  var map = new google.maps.Map(document.getElementById("map"),mapStart {
-    mapTypeID:'roadmap',
+    center:loc,
 	zoomControl: true,
+    mapTypeId:'roadmap'
   });
-	
+  
+  const marker = new google.maps.Marker({
+    position: loc,
+    map: map
+  });
+
+  const info = new google.maps.InfoWindow({
+    content: "<h2>This is a map</h2>" // Feature 3
+  });
+
+  marker.addListener("click", () => {
+    info.open(map, marker);
+  });
 }
