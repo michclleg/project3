c97f464314610757d8304cdce3ddf8116d3032f0
michclleg <mgonzalez18@hawk.iit.edu>
2025-04-22 17:03:26 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function initMap(){
   const loc={lat:41, lng:-87};
 
-  const maps=new google.maps.Map(document.getElementById("map"),{
+  const map=new google.maps.Map(document.getElementById("map"),{
     zoom:15,
     center:loc,
 	zoomControl:true,
@@ -10,17 +10,18 @@ function initMap(){
   
   const marker=new google.maps.Marker({
     position:loc,
-    map:maps,
+    map:map,
 	title:"This is IIT",
 	icon: {
 		url:'images/nav.png',
 		scaledSize:new google.maps.Size(50,50),
-	}
+	},
   });
 
   const info=new google.maps.InfoWindow({
     content:"<h2>School Campus</h2><p>This is IIT's campus</p>",
   });
+  
   marker.addListener("click", () => {
     info.open(map, marker);
   });
