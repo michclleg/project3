8bdf9c568529633b468995843ca55c17d2394a19
michclleg <mgonzalez18@hawk.iit.edu>
2025-04-21 19:25:06 -0500

    Update script.js
    Adding ','

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 function map() {
-  const loc = { lat:41, lng:-87};
+  const loc = {lat:41, lng:-87};
 
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom:5,
     center:loc,
 	zoomControl: true,
-    mapTypeId:'roadmap'
+    mapTypeId:'roadmap',
   });
   
   const marker = new google.maps.Marker({
     position: loc,
-    map: map
+    map: map,
+	title: "This is IIT",
   });
 
   const info = new google.maps.InfoWindow({
