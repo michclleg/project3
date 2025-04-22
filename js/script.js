17f432894e2fe8fb095a933911de4c4235bec66f
michclleg <mgonzalez18@hawk.iit.edu>
2025-04-22 14:57:27 -0500

    adding sidebars

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function initMap(){
   const loc={lat:41, lng:-87};
 
   const maps=new google.maps.Map(document.getElementById("map"),{
-    zoom:5,
+    zoom:10,
     center:loc,
 	zoomControl:true,
     mapTypeId:'roadmap',
